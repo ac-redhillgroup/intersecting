8dd5c78c6ec63039ce935b3ab80ec9cc72f83cf0
RMRed <rmurray@redhillgroup.com>
2017-10-27 09:31:13 -0700

    import statement added

--- qmformat.py
+++ qmformat.py
@@ -1,10 +1,12 @@
+from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
 direction = {'N' : 1,'S' : 2, 'E' : 3, 'W' : 4, 'CW' : 5, 'CL' : 6, 'IN' : 7, 'OB' : 8}
 #loading worksheet
 wb = load_workbook(filename = 'westcat.xlsx')
-ws = wb.worksheets[0]
+ws = wb.worksheets[0] 
+
 #new worksheet
 nwb = Workbook()
 nws = nwb.worksheets[0]
