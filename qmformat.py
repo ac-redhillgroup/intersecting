3d0d4bb720b196206715bac229435e863bd4ba46
RMRed <rmurray@redhillgroup.com>
2017-10-27 16:10:50 -0700

    reading values

--- qmformat.py
+++ qmformat.py
@@ -2,12 +2,27 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
-direction = {'N' : 1,'S' : 2, 'E' : 3, 'W' : 4, 'CW' : 5, 'CL' : 6, 'IN' : 7, 'OB' : 8}
+#direction = {'N' : 1,'S' : 2, 'E' : 3, 'W' : 4, 'CW' : 5, 'CL' : 6, 'IN' : 7, 'OB' : 8}
 #loading worksheet
-wb = load_workbook(filename = 'westcat.xlsx')
-ws = wb.worksheets[0] 
+def export():
+	wb = load_workbook(filename = 'westcat.xlsx')
+	ws = wb.worksheets[0] 
+	rows = ws.max_row
+	cols = ws.max_column
+	arr = []
+	i = 1
+	for row in ws.rows:
+		r = []
+		for cell in row:
+			r.append(cell.value)
+		arr.append(r)
+	    # i = i+1
+	    # if i == 4:
+	    # 	break
+	print arr[5]
 
+export()
 #new worksheet
-nwb = Workbook()
-nws = nwb.worksheets[0]
-nws.title = "Westcat QM data"
+# nwb = Workbook()
+# nws = nwb.worksheets[0]
+# nws.title = "Westcat QM data"
