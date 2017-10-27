7906d4074ac612e36bae76c302fe1e7a453c7dee
RMRed <rmurray@redhillgroup.com>
2017-10-27 09:30:08 -0700

    new workbook created

--- qmformat.py
+++ qmformat.py
@@ -1,9 +1,11 @@
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 
-direction = {'N' : 1,'S' : 2, 'E' : 3, 'W' : 4, 'CW' : 5, 'CL' : 6}
-coming_from = {'work': 1,'' }
-
-wb = load_workbook(filename = 'results-survey458251.xlsx')
+direction = {'N' : 1,'S' : 2, 'E' : 3, 'W' : 4, 'CW' : 5, 'CL' : 6, 'IN' : 7, 'OB' : 8}
+#loading worksheet
+wb = load_workbook(filename = 'westcat.xlsx')
 ws = wb.worksheets[0]
-rows = ws.max_row
+#new worksheet
+nwb = Workbook()
+nws = nwb.worksheets[0]
+nws.title = "Westcat QM data"
